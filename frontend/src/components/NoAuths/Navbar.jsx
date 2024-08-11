b3d57f985bf5d62a9f9efbf672f4270859107068
Taqi <mohammadtaqi5626@gmail.com>
2024-08-12 03:55:48 +0530

    fixed the navbar2

--- frontend/src/components/NoAuths/Navbar.jsx
+++ frontend/src/components/NoAuths/Navbar.jsx
@@ -1,31 +1,3 @@
-// import React from 'react'
-// import { Link, NavLink } from 'react-router-dom';
-
-// export default function Navbar() {
-
-//     const navLinksStyle = ({ isActive }) =>
-//         `mr-3 md:mr-5 ${isActive ? "text-white" : "text-gray-300"} font-medium md:text-lg cursor-pointer`;
-
-//     return (
-//         <header>
-//             <div className="sticky mx-auto flex flex-col md:flex-row justify-between p-3 items-center w-11/12">
-//                 <Link to="/" className='mb-0 cursor-pointer'>
-//                     <span className='ml-3 text-3xl md:text-4xl font-bold text-gray-100'>TaskFlow</span>
-//                 </Link>
-//                 <nav className="text-base mx-auto md:mx-0 md:ml-auto">
-//                     <NavLink to="/" className={navLinksStyle}>
-//                         Home
-//                     </NavLink>
-//                     <NavLink to="/about" className={navLinksStyle}>
-//                         About
-//                     </NavLink>
-//                 </nav>
-//             </div>
-//         </header>
-//     )
-// }
-
-
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import close from "../../assets/close.png"
